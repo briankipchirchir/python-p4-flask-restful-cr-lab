734df49292319d36625df052e013592b393e2054
briankipchirchir <kbrianngetich@gmail.com>
2024-10-07 11:17:36 +0300

    starter code

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -17,11 +16,51 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        """Get all plants"""
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+
+    def post(self):
+        """Create a new plant"""
+        data = request.get_json()
+        new_plant = Plant(
+            name=data.get('name'),
+            image=data.get('image'),
+            price=data.get('price')
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 class PlantByID(Resource):
-    pass
+    def get(self, plant_id):
+        """Get a plant by ID"""
+        plant = Plant.query.get_or_404(plant_id)
+        return jsonify(plant.to_dict())
+
+    def put(self, plant_id):
+        """Update a plant by ID"""
+        plant = Plant.query.get_or_404(plant_id)
+        data = request.get_json()
         
+        plant.name = data.get('name', plant.name)
+        plant.image = data.get('image', plant.image)
+        plant.price = data.get('price', plant.price)
+        
+        db.session.commit()
+        return jsonify(plant.to_dict())
+
+    def delete(self, plant_id):
+        """Delete a plant by ID"""
+        plant = Plant.query.get_or_404(plant_id)
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response(jsonify({"message": "Plant deleted"}), 204)
+
+# Register the resources with the API
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import Numeric  
 
 db = SQLAlchemy()
 
@@ -7,3 +8,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100))
+    price = db.Column(Numeric(10, 2)) 
+
+    def __repr__(self):
+        return f'<Plant {self.name}>'
